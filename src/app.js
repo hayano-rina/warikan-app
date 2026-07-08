41f41fb191132e89b15706bc22f532de2bce5aa9
rina <rina5214hay@keio.jp>
2026-07-08 15:52:53 +0900

    feat: フロントエンドの静的ファイル配信を追加

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config(); // .envファイルから環境変数を読み込む
 
 const routes = require("./routes"); // src/routes/index.js を自動的に読み込む
@@ -13,11 +14,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors()); // フロントエンドからのクロスドメイン通信を許可
 app.use(express.json()); // フロントエンドから送られてくるJSONデータを解析できるようにする
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 // ==========================================
 // ルーティングの設定
 // ==========================================
 // すべてのAPIの先頭に「/api」をつけるのが一般的です（例: /api/groups/:id/settlements）
 app.use("/api", routes);
+module.exports = app;
 
 // ==========================================
 // サーバーの起動
